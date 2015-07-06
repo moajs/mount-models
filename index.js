87d6256f7861fbaa00b31d6fa6eaa9a43c30dc36
alfred sang <i5ting@126.com>
2015-07-06 10:29:09 +0800

    bump version

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var requireDirectory = require('require-directory');
 
 function m(dir) {
   var a = dir.split('app')  
+  
   if(a.length !== 2){
+    a = dir.split('node_modules');
+  }else{
     throw  "mount-models ERROR: " + dir + "里没有node_modules目录";
   }
+  
   var _dir = a[0] + "app/models";
   return requireDirectory(module, _dir);
 }
